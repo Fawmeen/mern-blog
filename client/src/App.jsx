e387b29ef0e436d8f4e27e10051866f95ba58958
fawmeen <mohamedfawmeen76@gmail.com>
2024-11-29 23:44:33 +0530

    created page and pushed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './components/home'
+import About from './components/about'
+import Dashboard from './components/dashboard'
+import Projects from './components/projects'
+import Signin from './components/signin'
+import Signup from './components/signup'
+
 
 export default function App() {
   return (
-    <div>App</div>
+   <BrowserRouter>
+           <Routes>   <Route path="/" element={<Home />} />
+                      <Route path="/about" element={<About />} />
+                      <Route path="/signin" element={<Signin/>} />
+                      <Route path="/signup" element={<Signup />} />
+                      <Route path="/dashboard" element={<Dashboard />} />
+                      <Route path="/projects" element={<Projects/>} />  
+           </Routes>
+   </BrowserRouter>
   )
 }
